4f60cb23cc762b1ec76f326e15a43c551e4b4602
AERENE <Mugaadhiambo19@students.ku.ac.ke>
2022-03-14 01:59:59 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 char lower_case;
-for (lower_case = 48; lower_case <=57; lower_case++)
+for (lower_case = 48; lower_case <= 57; lower_case++)
 {
 putchar(lower_case);
 }
-for (lower_case = 97; lower_case <=102; lower_case++)
+for (lower_case = 97; lower_case <= 102; lower_case++)
 {
 putchar(lower_case);
 }
