60e845e17b6565e8a2837ab55dda93233966e65f
AERENE <Mugaadhiambo19@students.ku.ac.ke>
2022-03-14 01:46:13 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lower_case;
+for (lower_case = 122; lower_case >= 97; lower_case--)
+{
+putchar(lower_case);
+}
+putchar('/n');
+return (0);
+}
