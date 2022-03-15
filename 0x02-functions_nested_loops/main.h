743e67abf4ce8de95108f9ac32ba25a70549f2a1
AERENE <Mugaadhiambo19@students.ku.ac.ke>
2022-03-15 07:01:43 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,21 @@
-#ifndef main.h
-#define main.h
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints _putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-void fputs();
-
-#endif
+	return (0);
+}
