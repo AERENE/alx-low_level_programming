9a200e84dc414f86ddb328babda68016f57d9714
AERENE <mugaadhiambo19@students.ku.ac.ke>
2022-03-18 01:24:00 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - Returns 1 if c is uppercase
+ * @c: compares the digits
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+if (c > 'A' && c < 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
