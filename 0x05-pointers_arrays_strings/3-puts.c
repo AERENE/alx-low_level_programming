c6f62c605d7cce5606f7a7918eea3d07c59fa417
AERENE <mugaadhiambo19@students.ku.ac.ke>
2022-03-21 03:43:19 -0700

    Prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: The string to print
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+char *str;
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+putchar(str);
+putchar('\n');
+
+}
