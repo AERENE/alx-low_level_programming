33b182d2495bcd2681f0f0dff55c8392c808b607
AERENE <mugaadhiambo19@students.ku.ac.ke>
2022-03-21 06:24:16 -0700

    Prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,15 +4,17 @@
  * @str: The string to print
  * Return: Always 0.
  */
+
 void _puts(char *str)
 {
+	while (*str != '\0')
 
-while (*str != '\0')
+			{
 
-		{
-			_putchar(*str++);
+						_putchar(*str++);
 
-				}
-putchar('\n');
+							}
+
+		_putchar('\n');
 
 }
