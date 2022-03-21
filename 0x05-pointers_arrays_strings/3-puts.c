220944e9dd1a55b0cf56f36796dea11f43141393
AERENE <mugaadhiambo19@students.ku.ac.ke>
2022-03-21 06:30:41 -0700

    Prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,10 @@
 #include "main.h"
 /**
  * _puts - prints a string
- * @str: The string to print
+ * @str: The string
  * Return: Always 0.
  */
-
 void _puts(char *str)
 {
-	while (*str != '\0')
-
-			{
-
-						_putchar(*str++);
-
-							}
-
-		_putchar('\n');
-
+	printf("%s\n", str);
 }
