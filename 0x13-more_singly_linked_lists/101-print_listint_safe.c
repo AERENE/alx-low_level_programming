313d23fd5eb4af51d7a280ec747612452dbb132a
AERENE <99390318+AERENE@users.noreply.github.com>
2022-04-25 21:54:00 +0300

    prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,26 +1,29 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - function that prints a linked list with a loop
- * @head: head of the linked list
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
  *
- * Return: number of nodes of the linked list
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	if (head)
+	size_t num = 0;
+	long int diff;
+
+	while (head)
 	{
+		diff = head - head->next;
+		num++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (head->next < head)
-		{
-			return (1 + print_listint_safe(head->next));
-		}
+		if (diff > 0)
+			head = head->next;
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			return (1);
+			break;
 		}
-
 	}
-	return (0);
-}[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B
+
+	return (num);
+}
